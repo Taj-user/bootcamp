03520e170cb06a99e93672ef63935a088216e039
Taj-user <robtajzzz@icloud.com>
2026-05-22 00:52:36 -0500

    Finished qsort. However needs to be refactored

--- C/practice/sort.c
+++ C/practice/sort.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-// Creating a struct
+// Defining a struct
 typedef struct Person {
 	char name[50];
 	int age;
@@ -21,6 +21,8 @@ static void swap_element(void* base, size_t a, size_t b, size_t elem_size) {
 		*(j + k) = temp;
 	}
 }
+
+// Pivot calculation
 static void pivot(void* base, size_t n, size_t elem_size, int (*comparator)(const void*, const void*)) {
 	size_t first = 0;
 	size_t mid = n / 2;
@@ -35,6 +37,8 @@ static void pivot(void* base, size_t n, size_t elem_size, int (*comparator)(cons
 	// Move median to last
 	swap_element(base, mid, last, elem_size);
 }
+
+// Partition calculation
 static size_t partition(void* base, size_t n, size_t elem_size, int (*comparator)(const void*, const void*)) {
 	// Put pivot at last element
 	char* pivot = get_element(base, n - 1, elem_size);
@@ -52,43 +56,46 @@ static size_t partition(void* base, size_t n, size_t elem_size, int (*comparator
 }
 
 // Sorting Functions
-int compare_int_asc(const void* a, const void* b) {
-	return *(const int*)a - *(const int*)b;
-}
 
-int compare_int_desc(const void* a, const void* b) {
-	return *(const int*)b - *(const int*)a;
-}
+// Sort ints in ascending order
+int compare_int_asc(const void* a, const void* b) { return *(const int*)a - *(const int*)b; }
 
-float compare_float_asc(const void* a, const void* b) {
-	return *(const float*)a - *(const float*)b;
-}
+// Sort ints in descending order
+int compare_int_desc(const void* a, const void* b) { return *(const int*)b - *(const int*)a; }
 
-float compare_float_desc(const void* a, const void* b) {
-	return *(const float*)b - *(const float*)a;
-}
+// Sort floats in ascending order
+float compare_float_asc(const void* a, const void* b) { return *(const float*)a - *(const float*)b; }
 
+// Sort floats in descending order
+float compare_float_desc(const void* a, const void* b) { return *(const float*)b - *(const float*)a; }
+
+// Sort strings in ascending order
 int compare_string_asc(const void* a, const void* b) {
 	const char* sa = *(const char**)a;
 	const char* sb = *(const char**)b;
 	return strcmp(sa, sb);
 }
 
+// Sort strings in descending order
 int compare_string_desc(const void* a, const void* b) {
 	const char* sa = *(const char**)a;
 	const char* sb = *(const char**)b;
 	return strcmp(sb, sa);
 }
 
-int compare_struct_asc(const void* a, const void* b) {
-	return ((const Person*)a)->age - ((const Person*)b)->age;
+// Sort string field in struct in ascending order
+int compare_struct_string_asc(const void* a, const void* b) {
+	const char* sa = ((const Person*)a)->name;
+	const char* sb = ((const Person*)b)->name;
+	return strcmp(sa, sb);
 }
 
-int compare_struct_desc(const void* a, const void* b) {
-	return ((const Person*)b)->age - ((const Person*)a)->age;
+// Sort string field in struct in descending order
+int compare_struct_string_desc(const void* a, const void* b) {
+	const char* sa = ((const Person*)a)->name;
+	const char* sb = ((const Person*)b)->name;
+	return strcmp(sb, sa);
 }
-
-// TODO: Write sorting for string field in Person struct
 
 // qsort from scratch
 void my_qsort(void* array, size_t n, size_t elem_size, int (*comparator)(const void*, const void*)) {
@@ -121,7 +128,7 @@ int main(void) {
 
 	size_t size = sizeof(persons) / sizeof(persons[0]);
 
-	my_qsort(persons, size, sizeof(Person), compare_struct_asc);
+	my_qsort(persons, size, sizeof(Person), compare_struct_string_asc);
 
 	for(size_t i = 0; i < size; i++) {
 		printf("%s\t%d\n", persons[i].name, persons[i].age);
