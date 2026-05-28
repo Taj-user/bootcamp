95a7d1d71dcce5ec216087eec87ee33562f046fc
Taj-user <robtajzzz@icloud.com>
2026-05-28 16:03:26 -0500

    Main done. Need to test

--- C++/projects/gradebook/main.cpp
+++ C++/projects/gradebook/main.cpp
@@ -23,16 +23,22 @@ int main(void) {
 				std::string name;
 				std::cout << "Enter the name of the student you would like to add: ";
 				std::cin >> name;
+
 				addStudent(name);
 				std::cout << "Student " << name << " added successfully\n";
+
+				break;
 			}
 
 			case 2: {
 				std::string courseCode;
 				std::cout << "Enter the course code you would like to add: ";
 				std::cin >> courseCode;
+
 				addCourse(courseCode);
 				std::cout << "Course " << courseCode << " added successfully\n";
+
+				break;
 			}
 
 			case 3: {
@@ -48,14 +54,67 @@ int main(void) {
 					std::cout << "Student " << studentId << " is now enrolled in " << courseCode << "\n";
 				}
 				catch(const std::out_of_range& oore) { oore.what(); }
+
+				break;
 			}
 
 			case 4: {
-				// TODO
+				int studentId
+				std::string courseCode;
+				double grade;
+
+				std::cout << "Enter the id of the student: ";
+				std::cin >> studentId;
+				std::cout << "Enter the course code: ";
+				std::cin >> courseCode;
+				std::cout << "Enter the student's grade for the course: ";
+				std::cin >> grade;
+
+				try {
+					recordGrade(studentId, courseCode, grade);
+					std::cout << "Grade recorded successfully\n";
+				}
+				catch(const std::out_of_range& oore) { oore.what(); }
+
+				break;
+			}
+
+			case 5: {
+				printAllTranscripts();
+
+				break;
+			}
+
+			case 6: {
+				std::string courseCode;
+				std::cout << "Enter the course code: ";
+				std::cin >> courseCode;
+
+				printCourseRoster(courseCode);
+
+				break;
+			}
+
+			case 7: {
+				getTopStudent();
+
+				break;
+			}
+			
+			case 8: {
+				std::cout << "\nExiting...\n";
+
+				break;
+			}
+			
+			default: {
+				std::cout << "Invalid choice\n";
+
+				break;
 			}
 		}
-
-
 	}
 	while(choice != 8);
+
+	return 0;
 }
