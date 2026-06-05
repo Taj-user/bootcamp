c753c389fe24b27de461f5344cde0f1be9bc2a11
Taj-user <robtajzzz@icloud.com>
2026-06-04 23:53:43 -0500

    Classes and methods done. Need to test

--- C++/projects/taskqueue/TaskQueue.cpp
+++ C++/projects/taskqueue/TaskQueue.cpp
@@ -1,25 +1,36 @@
+#include <iostream>
 #include "TaskQueue.h"
 
 TaskQueue::TaskQueue()
-    : running_ = false;
+    : running_ (false)
 {}
 
 void TaskQueue::workerLoop() {
     while(running_) {
-        std::lock_guard<std::mutex> guard(mtx_);
+        std::unique_lock<std::mutex> lock(mtx_);
         if(!taskqueue_.empty()) {
             std::function<void()> front_task = taskqueue_.front();
             taskqueue_.pop();
+            lock.unlock();
+            front_task();
         }
-
+        else {
+            lock.unlock();
+        }
     }
 }
 
 void TaskQueue::enqueue(std::function<void()> task) {
-    std::lock_guard<std::mutex> guard(mtx_);
+    std::unique_lock<std::mutex> lock(mtx_);
     taskqueue_.push(task);
+    lock.unlock();
 }
 
 void TaskQueue::start() {
-
+    running_ = true;
+    t_ = std::thread(&TaskQueue::workerLoop, this);
+}
+void TaskQueue::stop() {
+    running_ = false;
+    t_.join();
 }
