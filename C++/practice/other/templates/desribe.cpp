cec7ccbda7bbfd87d577a6335724beb574f2c25b
Taj-user <robtajzzz@icloud.com>
2026-06-21 13:26:38 -0500

    Concepts practice

--- C++/practice/other/templates/desribe.cpp
+++ C++/practice/other/templates/desribe.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
+#include <concepts>
 #include <type_traits>
 
 template<typename T>
-std::enable_if_t<std::is_pointer_v<T>, void> describe(T value) {
+concept Pointer = std::is_pointer_v<T>;
+
+template<typename T>
+requires Pointer<T>
+void describe(T value) {
     std::cout << "pointer to: " << *value << "\n";
 }
 
 template<typename T>
-std::enable_if_t<!std::is_pointer_v<T>, void> describe(T value) {
+requires (!Pointer<T>)
+void describe(T value) {
     std::cout << "value: " << value << "\n";
 }
 
 int main(void) {
     int n = 42;
     int* p = &n;
-    describe<int*>(p);
-    describe<int>(n);
+    describe(p);
+    describe(n);
     return 0;
 }
