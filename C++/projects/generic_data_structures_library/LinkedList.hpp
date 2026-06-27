888a38756f1cf418e1b459f5ca29e9591576bf71
Taj-user <robtajzzz@icloud.com>
2026-06-26 19:16:04 -0500

    Basic generic linkedlist class template

---
+++ C++/projects/generic_data_structures_library/LinkedList.hpp
@@ -0,0 +1,82 @@
+#include <cstddef>
+#include <stdexcept>
+
+template<typename T>
+class LinkedList {
+        public:
+                LinkedList()
+                        : head(nullptr)
+                        , tail(nullptr)
+                {}
+
+                ~LinkedList() {
+                        while(!empty()) {
+                                pop_front();
+                        }
+                }
+
+                void push_back(T value) {
+                        if(head == nullptr && tail == nullptr) {
+                                Node* n = new Node;
+                                n->data = value;
+                                n->next = nullptr;
+                                head = n;
+                                tail = n;
+                        }
+                        else {
+                                Node* n = new Node;
+                                n->data = value;
+                                n->next = nullptr;
+                                tail->next = n;
+                                tail = n;
+                        }
+                }
+
+                void pop_front() {
+                        if(head == nullptr && tail == nullptr) throw std::out_of_range("No elements to pop");
+                        else {
+                                if(head->next == nullptr) {
+                                        Node* temp = head;
+                                        head = nullptr;
+                                        tail = nullptr;
+                                        delete temp;
+                                }
+                                else {
+                                        Node* temp = head;
+                                        head = temp->next;
+                                        delete temp;
+                                }
+                        }
+                }
+
+                T& front() {
+                        return head->data;
+                }
+
+                const T& front() const {
+                        return head->data;
+                }
+
+                size_t size() {
+                        if(head == nullptr && tail == nullptr) return 0;
+                        size_t count = 1;
+                        Node* i = head;
+                        while(i->next != nullptr) {
+                                i = i->next;
+                                count++;
+                        }
+                        return count;
+                }
+
+                bool empty() {
+                        return head == nullptr && tail == nullptr;
+                }
+
+        private:
+                struct Node {
+                        T data;
+                        Node* next;
+                };
+                Node* head;
+                Node* tail;
+};
