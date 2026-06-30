abc68e379bf57eed3d7428d34d3bcea9034ddbd9
Taj-user <robtajzzz@icloud.com>
2026-06-30 11:03:31 -0500

    Got a window working with quit logic

---
+++ C++/projects/2d_game_engine/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <SDL2/SDL.h>
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[]) {
+        if(SDL_Init(SDL_INIT_VIDEO) != 0) {
+                std::cerr << "SDL_Init failed: " << SDL_GetError() << "\n";
+                return 1;
+        }
+
+        SDL_Window* window = SDL_CreateWindow(
+                        "SDL2 Test",
+                        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+                        800, 600,
+                        SDL_WINDOW_SHOWN
+                        );
+
+        if(!window) {
+                std::cerr << "SDL_CreateWindow failed: " << SDL_GetError() << "\n";
+                SDL_Quit();
+                return 1;
+        }
+
+        bool running = true;
+        double accumulator {0.0};
+        Uint32 previous_time = SDL_GetTicks();
+        double FIXED_TIMESTAMP = 16.6666;
+
+        while(running) {
+                Uint32 current_time = SDL_GetTicks();
+                Uint32 delta_time = current_time - previous_time;
+                previous_time = current_time;
+                accumulator += delta_time;
+                SDL_Event event;
+                while(SDL_PollEvent(&event)) {
+                        if(event.type == SDL_QUIT) running = false;
+                }
+                while(accumulator >= FIXED_TIMESTAMP) {
+                        // update(FIXED_TIMESTAMP);
+                        accumulator -= FIXED_TIMESTAMP;
+                }
+                // render();
+        }
+
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return 0;
+}
