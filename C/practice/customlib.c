24079fb376f41bce91348e9874fbd22a5ac342bb
Taj-user <robtajzzz@icloud.com>
2026-07-03 10:58:40 -0500

    feature: custom strstr

--- C/practice/customlib.c
+++ C/practice/customlib.c
@@ -56,38 +56,28 @@ int my_strcmp(const char* str1, const char* str2) {
 	return str1_ascii - str2_ascii;
 }
 
-// // Timer
-// long long now_microseconds() {
-// 	struct timeval tv;
-// 	gettimeofday(&tv, NULL);
-//
-// 	return (long long)tv.tv_sec * 1000000LL + tv.tv_usec;
-// }
-//
-// // Benchmark
-// void benchmark(const char* name, size_t (*func)(const char*), const char* arg) {
-// 	long long start = now_microseconds();
-// 	volatile size_t sink = 0;
-//
-// 	for(int i = 0; i < RUNS; i++) {
-// 		sink += func(arg);
-// 	}
-//
-// 	long long end = now_microseconds();
-//
-// 	printf("%s: %lld us\n", name, end - start);
-// }
+// Custom strstr
+const char* my_strstr(const char* haystack, const char* needle) {
+        if(*needle == '\0') return haystack;
+        while(*haystack) {
+                const char* h = haystack;
+                const char* n = needle;
+
+                while(*h && *n && *h == *n) {
+                        ++h;
+                        ++n;
+                }
+
+                if(*n == '\0') return haystack;
+                ++haystack;
+        }
+        return NULL;
+}
 
 
 int main(void) {
-	// const char* test = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,";
-	char bread[] = "Bread";
-	char butter[] = " Butter";
-
-	int cmp = my_strcmp(bread, butter);
-	if(cmp > 0) printf("%s", bread);
-	else if(cmp < 0) printf("%s", butter);
-	else printf("Both strings equal\n");
+        const char* str = my_strstr("Riddle me this, Riddle me that", "this");
+        if(str != NULL) printf("%s", str);
 
 	return 0;
 }
