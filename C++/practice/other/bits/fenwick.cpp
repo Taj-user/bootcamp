ee7d52bb2b6bae0705668e808c37ff0c42d4cf1e
Taj-user <robtajzzz@icloud.com>
2026-07-06 21:20:41 -0500

    practice: Fenwick tree fully implemented

--- C++/practice/other/bits/fenwick.cpp
+++ C++/practice/other/bits/fenwick.cpp
@@ -4,7 +4,21 @@ struct FenwickTree {
         int n;
         std::vector<int> tree;
 
-        FenwickTree(int n);
-        void update(int i, int delta);
-        int query(int i);
+        FenwickTree(int n) : n(n), tree(n + 1) {}
+
+        void update(int i, int delta) {
+                while(i < n) {
+                        tree[i] += delta;
+                        i += (i & -i);
+                }
+        }
+
+        int query(int i) {
+                int sum = 0;
+                while(i != 0) {
+                        sum += tree[i];
+                        i -= (i & -i);
+                }
+                return sum;
+        }
 };
