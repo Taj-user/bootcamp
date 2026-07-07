30b81b070c6433e7a065a85a1c3ae45500e0595e
Taj-user <robtajzzz@icloud.com>
2026-07-07 17:22:10 -0500

    testing: Fenwick tree testing

--- C++/practice/other/bits/fenwick.cpp
+++ C++/practice/other/bits/fenwick.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <vector>
 
 struct FenwickTree {
@@ -22,3 +23,20 @@ struct FenwickTree {
                 return sum;
         }
 };
+
+int main(void) {
+        FenwickTree tree(8);
+        tree.update(1, 3);
+        tree.update(2, 2);
+        tree.update(3, 1);
+        tree.update(4, 5);
+        tree.update(5, 4);
+        tree.update(6, 2);
+        tree.update(7, 3);
+        tree.update(8, 1);
+        std::cout << tree.query(3) << "\n";
+        std::cout << tree.query(6) << "\n";
+        tree.update(3, 2);
+        std::cout << tree.query(3) << "\n";
+        return 0;
+}
