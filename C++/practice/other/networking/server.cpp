0a53d92b0bab3548e2eccf735eb6555014729e5b
Taj-user <robtajzzz@icloud.com>
2026-07-08 17:00:22 -0500

    select() based implementation for handling multiple clients

--- C++/practice/other/networking/server.cpp
+++ C++/practice/other/networking/server.cpp
@@ -1,22 +1,24 @@
-#include <thread>
+// #include <thread>
 #include <iostream>
+#include <vector>
 #include <winsock2.h>
 #include <ws2tcpip.h>
 
 bool send_all(SOCKET fd, const char* buffer, int length);
 
-void handleClient(SOCKET clientfd) {
-        char buffer[1024];
-        int recvfd {};
-        while((recvfd = recv(clientfd, buffer, sizeof(buffer) - 1, 0)) > 0) {
-                buffer[recvfd] = '\0';
-                std::cout << buffer << "\n";
-                const char* response = "Message received";
-                send_all(clientfd, response, static_cast<int>(strlen(response)));
-        }
-
-        closesocket(clientfd);
-}
+// helper function for multithreaded approach to handling multiple clients
+// void handleClient(SOCKET clientfd) {
+//         char buffer[1024];
+//         int recvfd {};
+//         while((recvfd = recv(clientfd, buffer, sizeof(buffer) - 1, 0)) > 0) {
+//                 buffer[recvfd] = '\0';
+//                 std::cout << buffer << "\n";
+//                 const char* response = "Message received";
+//                 send_all(clientfd, response, static_cast<int>(strlen(response)));
+//         }
+//
+//         closesocket(clientfd);
+// }
 
 bool send_all(SOCKET fd, const char* buffer, int length) {
         int total = 0;
@@ -55,15 +57,64 @@ int main(void) {
         struct sockaddr_in client_addr;
         socklen_t client_len = sizeof(client_addr);
 
+        // select()
+        fd_set read_fds;
+        std::vector<SOCKET> connected_clients;
         while(true) {
-                SOCKET clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
-                if(clientfd == INVALID_SOCKET) {
-                        closesocket(sockfd);
-                        return -1;
+                FD_ZERO(&read_fds);
+                FD_SET(sockfd, &read_fds);
+                SOCKET max_fd = sockfd;
+
+                for(SOCKET fd : connected_clients) {
+                        FD_SET(fd, &read_fds);
+                        if(fd > max_fd) {
+                                max_fd = fd;
+                        }
                 }
-                std::thread t(handleClient, clientfd);
-                t.detach();
+
+                select(max_fd + 1, &read_fds, nullptr, nullptr, nullptr);
+
+                if(FD_ISSET(sockfd, &read_fds)) {
+                        SOCKET clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
+                        if(clientfd == INVALID_SOCKET) {
+                                closesocket(sockfd);
+                                return -1;
+                        }
+                        connected_clients.push_back(clientfd);
+                }
+
+                for(auto it = connected_clients.begin(); it != connected_clients.end(); ) {
+                        if(FD_ISSET(*it, &read_fds)) {
+                                char buffer[1024];
+                                int bytes = recv(*it, buffer, sizeof(buffer) - 1, 0);
+                                if(bytes == 0) {
+                                        closesocket(*it);
+                                        it = connected_clients.erase(it);
+                                }
+                                else {
+                                        buffer[bytes] = '\0';
+                                        std::cout << buffer << "\n";
+                                        const char* response = "Message received";
+                                        send_all(*it, response, static_cast<int>(strlen(response)));
+                                        ++it;
+                                }
+                        }
+                        else {
+                                ++it;
+                        }
+                }
         }
+
+        // Multithreading approach to handling multiple clients
+        // while(true) {
+        //         SOCKET clientfd = accept(sockfd, (struct sockaddr*)&client_addr, &client_len);
+        //         if(clientfd == INVALID_SOCKET) {
+        //                 closesocket(sockfd);
+        //                 return -1;
+        //         }
+        //         std::thread t(handleClient, clientfd);
+        //         t.detach();
+        // }
 
         closesocket(sockfd);
 
