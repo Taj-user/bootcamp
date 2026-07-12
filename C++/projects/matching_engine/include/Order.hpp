dcbdd5b3351c4f373200b23b49ea79a769f06844
Taj-user <robtajzzz@icloud.com>
2026-07-12 18:16:02 -0500

    refactor: ring buffer is more thread safe

--- C++/projects/matching_engine/include/Order.hpp
+++ C++/projects/matching_engine/include/Order.hpp
@@ -1,7 +1,5 @@
 #pragma once
 #include "Types.h"
-#include <winsock2.h>
-#include <ws2tcpip.h>
 
 namespace OrderFlags {
         constexpr u8 BUY        = 1 << 0;
@@ -14,7 +12,6 @@ namespace OrderFlags {
 struct Order {
         u64     order_id;
         double  price;
-        SOCKET  client;
         u32     quantity;
         u8      side = OrderFlags::BUY;
 };
@@ -23,8 +20,6 @@ struct MatchResult {
         u64     bid_order_id;
         u64     ask_order_id;
         double  price;
-        SOCKET  bid_client;
-        SOCKET  ask_client;
         u32     match_qty;
 };
 
